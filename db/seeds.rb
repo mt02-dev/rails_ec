fd131b38bec275602989e1196619d99f2aa0ca2b
mt02-dev <t.m.215soccer@icloud.com>
2024-02-11 21:41:19 +0900

    robocop, htmlbeatifier実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,7 +8,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-product_count = Dir.children(Rails.root.join("app/assets/images/rails_ec_images/")).count
+product_count = Dir.children(Rails.root.join('app/assets/images/rails_ec_images/')).count
 product_count.times do |i|
   Product.create!(
     name: "Test Product #{i + 1}",
