d0b0f57d08be69ef9cb1796930442926c4dc2724
mt02-dev <t.m.215soccer@icloud.com>
2024-02-27 22:29:28 +0900

    excuted rubocop & htmlbeautifier

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,6 @@ class ProductsController < ApplicationController
   end
 
   def show
-    debugger
     @product = Product.find(params[:id])
     @related_products = Product.order(:created_at).limit(4)
   end
