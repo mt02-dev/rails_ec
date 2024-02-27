affcfe19d5eed9b63de75edfaa72604293e4cce5
mt02-dev <t.m.215soccer@icloud.com>
2024-02-27 22:26:46 +0900

    商品詳細画面実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,9 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def show; end
+  def show
+    debugger
+    @product = Product.find(params[:id])
+    @related_products = Product.order(:created_at).limit(4)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
   resources :tasks
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
