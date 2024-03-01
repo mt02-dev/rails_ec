99cd5748ff45399259f3f0da3a2a62c4ac03aaa6
mt02-dev <t.m.215soccer@icloud.com>
2024-03-01 11:30:47 +0900

    商品管理機能実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@
 
 class Product < ApplicationRecord
   has_one_attached :product_image
+  validates :name, presence: true
+  validates :price, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
 end
