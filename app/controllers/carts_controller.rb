b6815f817791ccc073a0c50cb61a08092df0e78c
mt02-dev <t.m.215soccer@icloud.com>
2024-03-08 11:58:03 +0900

    テーブルを用いたカート機能の実装

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,34 +1,50 @@
-class CartsController < ApplicationController
+# frozen_string_literal: true
 
-  def index; end
-  
+class CartsController < ApplicationController
+  def index
+    product_id_and_quantity = CartProduct.where(cart_id: session[:cart_id]).group(:product_id).sum(:quantity)
+
+    @cart = { products: [], total:  0 }
+    product_id_and_quantity.each do |product_id, quantity|
+      product = Product.find(product_id)
+      @cart[:total] += product.price * quantity
+      @cart[:products] << { product:, quantity: }
+    end
+    @cart
+  end
+
   def new; end
-  
+
   def create
-    # この状態だと別のカートが作れない 異なるブラウザ間でカート共有している状態
     create_cart if session[:cart_id].nil?
-    @product = Product.find(params[:product_id])
-    cart_item = CartProduct.new(cart_id: session[:cart_id], product_id: @product.id,quantity: params[:quantity])
+
+    # add_to_cartボタンが押されたオブジェクトを検索
+    product = Product.find(params[:product_id])
+    cart_item = CartProduct.new(cart_id: session[:cart_id], product_id: product.id, quantity: params[:quantity])
     if cart_item.save
-      specify_destination(@product)
+      params[:flag] == 'main' ? specify_destination(product) : specify_destination(params[:main_product])
     else
       flash[:danger] = "Couldn't add in cart."
     end
   end
-    
+
+  def destroy
+    CartProduct.where(cart_id: session[:cart_id]).where(product_id: params[:target]).destroy_all
+    redirect_to carts_path status: :see_other
+  end
 
   private
-  
-  def create_cart 
+
+  def create_cart
     cart = Cart.create!
     session[:cart_id] = cart.id
   end
 
   def specify_destination(product)
-    if session[:location] == 'show'
-    else 
-      render product_url(product)
+    if params[:location] == 'show'
+      redirect_to product_url(product)
+    else
+      redirect_to products_url
     end
   end
-
 end
--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 module CartsHelper
-  def cart_products
-    CartProduct.count
+  def cart_products(cart_id)
+    return 0 if cart_id.nil?
+
+    CartProduct.where(cart_id:).sum(:quantity)
   end
 end
